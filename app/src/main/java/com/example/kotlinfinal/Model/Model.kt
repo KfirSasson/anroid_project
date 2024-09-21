7ca3559657da884fcc27208f6f8c5360a86c5986
KfirSasson <kfir.s.sasson@gmail.com>
2024-09-21 14:22:36 +0300

    Row and listview creation, next is recyclerview.

---
+++ app/src/main/java/com/example/kotlinfinal/Model/Model.kt
@@ -0,0 +1,18 @@
+package com.example.kotlinfinal.Model
+
+class Model {
+
+    val trainings: MutableList<Training> = ArrayList()
+    companion object {
+        val instance: Model = Model()
+    }
+
+    init {
+        for (i in 0..20){
+            val training = Training("$i",
+                "$i",
+                "avatar.png")
+            trainings.add(training)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/kotlinfinal/Model/Training.kt
@@ -0,0 +1,4 @@
+package com.example.kotlinfinal.Model
+
+data class Training(val type: String,
+    val description: String, val avatar: String)
